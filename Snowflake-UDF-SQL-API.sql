c08df04206d490aa1aadea214f389d8436e42c87
Michael Maxi <107141374+michaelmaxi@users.noreply.github.com>
2023-01-05 15:29:44 +1000

    Add files via upload

---
+++ Snowflake-UDF-SQL-API.sql
@@ -0,0 +1,93 @@
+-- Set context 
+USE ROLE ACCOUNTADMIN;
+USE WAREHOUSE COMPUTE_WH;
+
+-- Create demo database and schema
+CREATE DATABASE DEMO_DB;
+CREATE SCHEMA DEMO_SCHEMA;
+
+--Set context
+USE DATABASE DEMO_DB;
+USE SCHEMA DEMO_SCHEMA;
+
+--User Defined Functions
+
+-- SQL UDF to return the name of the day of the week on a date in the future
+CREATE OR REPLACE FUNCTION DAY_NAME_ON(num_of_days int)
+RETURNS STRING
+  AS
+  $$
+    select 'In ' || CAST(num_of_days AS string) || ' days it will be a ' || dayname(dateadd(day,num_of_days, current_date()))
+  $$; 
+  -- single quote can be used instead of dollar sign to delimit function body
+  
+  
+-- Use the SQL UDF as part of a query. 
+SELECT DAY_NAME_ON(100);
+
+
+-- External Function 
+  
+CREATE OR REPLACE API INTEGRATION demonstration_external_api_integration_01
+    API_PROVIDER=aws_api_gateway
+    API_AWS_ROLE_ARN='arn:aws:iam::123456789012:role/my_cloud_account_role'
+    API_ALLOWED_PREFIXES=('https://xyz.execute-api.us-west-2.amazonaws.com/production')
+    ENABLED=true;
+
+CREATE OR REPLACE EXTERNAL FUNCTION local_echo(string_col varchar)
+    RETURNS variant
+    API_INTEGRATION = demonstration_external_api_integration_01 -- API Integration object
+    AS 'https://xyz.execute-api.us-west-2.amazonaws.com/production/remote_echo'; -- Proxy service URL
+
+SELECT my_external_function(34, 56);
+
+-- Stored procedure JavaScript
+
+-- Create demo tables and insert data to test procedure
+CREATE TABLE DEMO_TABLE 
+(
+NAME STRING, 
+AGE INT
+);
+
+CREATE TABLE DEMO_TABLE_2 
+(
+NAME STRING, 
+AGE INT
+);
+    
+INSERT INTO DEMO_TABLE VALUES ('Edric',56),('Jayanthi',23),('Chloe',51),('Rowland',50),('Lorna',33),('Satish',19);
+INSERT INTO DEMO_TABLE_2 VALUES ('Edric',56),('Jayanthi',23),('Chloe',51),('Rowland',50),('Lorna',33),('Satish',19);
+
+SELECT COUNT(*) FROM DEMO_TABLE;
+SELECT COUNT(*) FROM DEMO_TABLE_2;
+
+
+CREATE OR REPLACE PROCEDURE TRUNCATE_ALL_TABLES_IN_SCHEMA(DATABASE_NAME STRING, SCHEMA_NAME STRING)
+    RETURNS STRING
+    LANGUAGE JAVASCRIPT
+    EXECUTE AS OWNER -- can also be executed as 'caller'
+    AS
+    $$
+    var result = [];
+    var namespace = DATABASE_NAME + '.' + SCHEMA_NAME;
+    var sql_command = 'SHOW TABLES in ' + namespace ; 
+    var result_set = snowflake.execute({sqlText: sql_command});
+    while (result_set.next()){
+        var table_name = result_set.getColumnValue(2);
+        var truncate_result = snowflake.execute({sqlText: 'TRUNCATE TABLE ' + table_name});
+        result.push(namespace + '.' + table_name + ' has been sucessfully truncated.');
+        
+    }
+    return result.join("\n"); 
+    $$;
+
+
+-- Calling a stored procedure cannot be used as part of a SQL statement, dissimilar to a UDF. 
+CALL TRUNCATE_ALL_TABLES_IN_SCHEMA('DEMO_DB', 'DEMO_SCHEMA');
+
+SELECT COUNT(*) FROM DEMO_TABLE;
+SELECT COUNT(*) FROM DEMO_TABLE_2;
+
+-- Clear-down objects
+DROP DATABASE DEMO_DB;
